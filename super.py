f4cfd1e6daa9b6a633f80d7ae985f6cb7eaa1e0f
Vamshi Verma <vamshipaili@outlook.com>
2022-10-15 23:21:27 -0400

    Update super.py

--- super.py
+++ super.py
@@ -46,9 +46,9 @@ def get_tok(inputURL):
     driver.find_element(By.XPATH,'//*[@id="submit"]').click()
 
     #Await the download page to load
-    time.sleep(10)
+    #time.sleep(10)
     #scroll down
-    driver.execute_script("window.scrollTo(0, 200)") 
+    #driver.execute_script("window.scrollTo(0, 200)") 
     st.info(driver.page_source)
 
 
