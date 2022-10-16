8ee7e8761f0e894fdb76c962965e6f01c17386d3
Vamshi Verma <vamshipaili@outlook.com>
2022-10-16 10:18:58 -0400

    Update superstreamlit.py

--- superstreamlit.py
+++ superstreamlit.py
@@ -5,7 +5,7 @@ st.markdown('# 👩‍🎓 **ZMatter**')
 war = st.empty()
 
 war.warning('⬆️ Awaiting URL input in the sidebar.')
-war.image("https://i.postimg.cc/pdxbQ4YJ/Hack.gif")
+war.image("https://i.postimg.cc/PJ108MpR/hack.gif")
 # Sidebar
 st.sidebar.header('Input parameter')
 
